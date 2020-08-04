104f7471fb3c9fc2cb0952df1297993ab8f71306
Paul Nitu <pnitu@airtouchmedia.com>
2020-08-04 11:00:34 +0300

    Create host fragment for navigation and add it to nav graph

---
+++ app/src/main/java/com/codingbatch/positivenews/NewsListFragment.kt
@@ -0,0 +1,19 @@
+package com.codingbatch.positivenews
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class NewsListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_news_list, container, false)
+    }
+}
