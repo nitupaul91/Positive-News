4387253476f834e8d8432c46dcc4a9aa4d056713
Paul Nitu <pnitu@airtouchmedia.com>
2020-08-04 12:41:03 +0300

    Create UI package and add fragment to it

--- app/src/main/java/com/codingbatch/positivenews/ui/newslist/NewsListFragment.kt
+++ app/src/main/java/com/codingbatch/positivenews/ui/newslist/NewsListFragment.kt
@@ -1,10 +1,11 @@
-package com.codingbatch.positivenews
+package com.codingbatch.positivenews.ui.newslist
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.codingbatch.positivenews.R
 
 class NewsListFragment : Fragment() {
 
