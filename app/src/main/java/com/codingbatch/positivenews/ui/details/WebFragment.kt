eb0fc1f4524d62a83942952ad1ad623cb4bb3669
Paul Nitu <pnitu@airtouchmedia.com>
2020-08-04 23:27:34 +0300

    Create WebFragment and layout and add it to nav graph

---
+++ app/src/main/java/com/codingbatch/positivenews/ui/details/WebFragment.kt
@@ -0,0 +1,24 @@
+package com.codingbatch.positivenews.ui.details
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class WebFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = super.onCreateView(inflater, container, savedInstanceState)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
